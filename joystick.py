65430452d85b40583094d894386ab34e50469680
medsal15 <medsal16+github@gmail.com>
2022-04-01 11:38:53 +0200

    Fix callable params

--- joystick.py
+++ joystick.py
@@ -27,7 +27,7 @@ class Joystick:
     (This part depends on pygame's goodwill)
     '''
 
-    _events : dict[int, list[Callable[event.EventType, None]]] = {}
+    _events : dict[int, list[Callable[list[event.EventType], None]]] = {}
     _id : int
 
     _axis : dict[int, float] = {}
